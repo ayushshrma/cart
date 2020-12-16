fa4d8248ceffa015945dbc28cbe66e5ee70a7fe8
Ayush Sharma <realayusharma@gmail.com>
2020-12-16 16:29:39 +0530

    adding style in component CartItem

--- src/CartItem.js
+++ src/CartItem.js
@@ -14,7 +14,9 @@ class CartItem extends React.Component{
                <div style={{color: 'GrayText'}}>Rs999</div>
                <div style={{color: 'GrayText'}}>quantity:1</div>
                <div className="cart-item-actions">
-        {/* for action buttons delete items*/}
+                   <img alt="increase" className="action-icons" src="https://www.flaticon.com/svg/static/icons/svg/992/992651.svg"/>
+                   <img alt="decrease" className="action-icons" src="https://www.flaticon.com/svg/static/icons/svg/992/992683.svg"/>
+                   <img alt="delete" className="action-icons" src="https://www.flaticon.com/svg/static/icons/svg/3096/3096673.svg"/>
                </div>
           </div>
     
